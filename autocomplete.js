7d76001c12e88d59a4a7f03006efda1638eceafc
Nootan Ghimire <nootan.ghimire@gmail.com>
2013-11-03 16:35:07 +0545

    Workable functionality w/o css

--- autocomplete.js
+++ autocomplete.js
@@ -1,9 +1,5 @@
-var config_autocomplete = {
-	path:"data.php",
-	getVar:"sent"
-}
-
-var autocomplete = {	
+var autocomplete = {
+	output:[],	
 	callAjax: function(path, value_pair, callback){
 		var Ajax = new XMLHttpRequest();
 		Ajax.onreadystatechange = function(){
@@ -15,25 +11,116 @@ var autocomplete = {
 		Ajax.open('GET',path + '?'+ value_pair, true);
 		Ajax.send();
 	},
-	complete: function(inputId, callback){
-		var inputObject = document.getElementById(inputId);
+	complete: function(config_autocomplete, callback){
+		var inputObject = document.getElementById(config_autocomplete.inputId);
 		var that = this; 
-		inputObject.addEventListener('keypress', function(){
-			// global config_autocomplete; (Only for readability)
-			var toSend = window.config_autocomplete.getVar + "="+ inputObject.value 
-			var path = window.config_autocomplete.path;
+		inputObject.addEventListener('keyup', function(){
+			var toSend = config_autocomplete.getVar + "="+ this.value
+			//alert(toSend);
+			var path = config_autocomplete.path;
 			that.callAjax(path, toSend, function(data){
 				var myData = JSON.parse(data);
-				callback(myData);
+				that.showList(config_autocomplete, myData, callback);
 			});	
 		});		
+	},
+	showList: function(config, data, callback){
+		var currentElement = document.getElementById(config.divList);
+		currentElement.innerHTML = "";
+        for (var i = data.length - 1; i >= 0; i--) {
+        	var innerChild = document.createElement('div');
+			innerChild.setAttribute('class', config.divListEach);
+        	for (var key in data[i]) {
+
+        		innerChild.setAttribute('data-'+key, data[i][key]);
+
+        	};
+
+			innerChild.innerHTML = data[i][config.innerHTMLkey];
+			currentElement.appendChild(innerChild);
+			var that = this;
+			toSendData = data[i];
+			innerChild.addEventListener('click', function(){
+				that.addTag(config, this);
+				callback(data, that.output);
+			});
+			
+		};
+	},
+	addTag: function(config, elemObj){
+		tagData = this.getDataset(elemObj);
+		if(this.in_array(tagData, this.output)){
+			//alert(this.output);
+			return false;
+		}
+		this.output.push(tagData);
+		var currentElement = document.getElementById(config.divTag);
+		//add element and append it
+		var tagElem = document.createElement('span');
+		for (var key in tagData) {
+        	tagElem.setAttribute('data-'+key, tagData[key]);
+        };
+		tagElem.setAttribute('class', config.elemClass);
+		tagElem.innerHTML = tagData[config.innerHTMLkey];
+		currentElement.appendChild(tagElem);
+	},
+
+	in_array: function (needle, haystack, argStrict) {
+	  // http://kevin.vanzonneveld.net
+	  // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	  // +   improved by: vlado houba
+	  // +   input by: Billy
+	  // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
+	  var key = '',
+	    strict = !! argStrict;
+
+	  if (strict) {
+	    for (key in haystack) {
+	      if (haystack[key] === needle) {
+	        return true;
+	      }
+	    }
+	  } else {
+	    for (key in haystack) {
+	      if (haystack[key] == needle) {
+	        return true;
+	      }
+	    }
+	  }
+	  return false;
+	},
+	getDataset: function(objHtmlElem){
+		if(objHtmlElem.dataset){
+			return objHtmlElem.dataset;
+		} else {
+			//Write another workaround
+		}
 	}
 }
 
+/* End Object */
 
-autocomplete.complete('complete', function(data){
-	for (var i = data.length - 1; i >= 0; i--) {
-		alert(data[i].id);
-		alert(data[i].name);
-	};
+
+//Example
+
+//Write a config;
+
+var config = {
+	inputId:'complete',
+	path:"data.php",
+	divList:"list-div",
+	divListEach:"list",
+	divTag:"tag-div",
+	elemClass:"tags",
+	getVar:"sent",
+	outputVar:"output",
+	innerHTMLkey:"name"
+}
+
+//call the method
+autocomplete.complete(config, function(data, output){
+	//alert(JSON.stringify(data));
+	alert(JSON.stringify(output));
+	//tada! you have the selected data and the returned JSON data to play with.
+	//called on every selection made.
 }); 
--- data.php
+++ data.php
@@ -1,5 +1,15 @@
 <?php
 if(isset($_GET['sent'])){
+	if($_GET['sent'] == 'a' ){
+		$toEcho[0]['id'] = 40;
+		$toEcho[0]['name']="Astapor";
+		$toEcho[1]['id'] = 80;
+		$toEcho[1]['name']="Alleypre";	
+		echo json_encode($toEcho);
+		exit();
+	} else if(trim($_GET['sent'])==""){
+		exit();
+	}
 	$toEcho[0]['id'] = 10;
 	$toEcho[0]['name']="Nepal";
 	$toEcho[1]['id'] = 20;
